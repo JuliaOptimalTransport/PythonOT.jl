b4fd301376cdd752a732ca67927685835ae69c99
David Widmann <dev+github@devmotion.de>
2021-05-06 15:21:47 +0200

    Rename file (possibly file name causes error on Windows an MacOS?)

--- src/POT.jl
+++ src/POT.jl
@@ -6,7 +6,7 @@ export emd, emd2, sinkhorn, sinkhorn2, sinkhorn_unbalanced, sinkhorn_unbalanced2
 
 const pot = PyCall.PyNULL()
 
-include("pot.jl")
+include("lib.jl")
 
 function __init__()
     return copy!(pot, PyCall.pyimport_conda("ot", "pot", "conda-forge"))
