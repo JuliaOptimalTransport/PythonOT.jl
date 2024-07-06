d861202fafc6fb599ee8dd64076c741609ed4aad
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-07-06 14:28:40 +0200

    Update src/lib.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/lib.jl
+++ src/lib.jl
@@ -532,7 +532,7 @@ W = \\min_{\\gamma \\geq 0} \\langle \\gamma, M \\rangle_F +
 where
 
 - `M` is the metric cost matrix,
-- ``\\mathbf{a}`` and ``\\mathbf{b}`` are source and target unbalanced distributions.
+- `a` and `b` are source and target unbalanced distributions,
 - ``\\mathbf{c}`` is a reference distribution for the regularization.
 - ``\\mathrm{reg_m}`` is the marginal relaxation term 
 
